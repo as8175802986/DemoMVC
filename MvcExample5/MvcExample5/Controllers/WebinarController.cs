84e63a958b657d6da8b9340e652e0dc1f2feaf7a
Sharma <arunesh.sharma@sagitec.com>
2022-07-11 17:23:27 +0530

    [Arunesh] added code for MVC Practice

---
+++ MvcExample5/MvcExample5/Controllers/WebinarController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcExample5.Controllers
+{
+    public class WebinarController : Controller
+    {
+        // GET: Webinar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(string FullName , string Age, string Contact, string Email)
+        {
+            TempData["FullName"] = FullName;
+            TempData["Age"] = Age;
+            TempData["Contact"] = Contact;
+            TempData["email"] = Email;
+            return RedirectToAction("Confirmation");
+            //return View();
+        }
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
